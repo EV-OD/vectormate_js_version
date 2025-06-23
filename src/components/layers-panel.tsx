ae203cc86f683f5c828fe550b4384e876c4da7a0
Evod lc <evod599@gmail.com>
2025-06-23 12:21:34 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div dat

--- src/components/layers-panel.tsx
+++ src/components/layers-panel.tsx
@@ -50,7 +50,7 @@ export function LayersPanel({ shapes, selectedShapeIds, onSelectShape, onDelete,
 
   return (
     <div className="h-full flex flex-col bg-card">
-      <ScrollArea className="flex-1">
+      <ScrollArea className="flex-1 min-h-0">
         <div className="p-2 space-y-1">
           {reversedShapes.map((shape) => (
             <div
